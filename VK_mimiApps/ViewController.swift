f24b9367ff00401b83476310b417e28325236812
JessiePinkMann <egor.anoshin.03@mail.ru>
2024-09-05 17:03:45 +0300

    Initial Commit

---
+++ VK_mimiApps/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VK_mimiApps
+//
+//  Created by Egor Anoshin on 05.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
